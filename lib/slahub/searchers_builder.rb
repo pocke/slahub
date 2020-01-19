ac752bc628175183ecd15eaea5c70b54d4e1df6d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-01-20 00:32:21 +0900

    watching dynamic query

--- lib/slahub/searchers_builder.rb
+++ lib/slahub/searchers_builder.rb
@@ -1,5 +1,11 @@
 module Slahub
   class SearchersBuilder
+    # GitHub does not accept too large URI,
+    # so it should separate requests if the query is too long.
+    # The limit is actually about 6800. I found this limit with binary searching with GitHub API.
+    # This constant has leeway.
+    GITHUB_URI_LIMIT_LENGTH = 5000
+
     def initialize(config:, github_client:)
       @config = config
       @github_client = github_client
@@ -9,13 +15,49 @@ module Slahub
       queries.flat_map do |q|
         query = q['query']
         channel = q['channel']
+        query_dynamic = q['query_dynamic']
 
+        if query
+          Searcher.new(
+            query: query,
+            github_client: @github_client,
+            issue_to_channel: -> (_issue) { channel }
+          )
+        elsif query_dynamic == 'watching'
+          build_watching channel: channel
+        else
+          raise "unknown query: #{q}"
+        end
+      end
+    end
+
+    private def build_watching(channel:)
+      repos = @github_client.v4.all_watching_repositories
+      queries = repositories_to_queries(repos)
+
+      queries.map do |q|
         Searcher.new(
-          query: query,
+          query: q,
           github_client: @github_client,
           issue_to_channel: -> (_issue) { channel }
         )
       end
+    end
+
+    private def repositories_to_queries(repositories)
+      q = +''
+      qs = []
+      repositories.each do |repo|
+        if q.length > GITHUB_URI_LIMIT_LENGTH
+          qs << q
+          q = +''
+        else
+          q << "repo:#{repo[:nameWithOwner]}"
+        end
+      end
+      qs << q unless q.empty?
+
+      qs
     end
 
     private def queries
--- test/slahub/searchers_builder_test.rb
+++ test/slahub/searchers_builder_test.rb
@@ -26,4 +26,24 @@ class SearcherBuilderTest < Minitest::Test
     assert_equal 2, searchers.size
     assert(searchers.all? { |s| s.is_a? Slahub::Searcher })
   end
+
+  def test_build_3
+    begin
+      config_base = Slahub::ConfigLoader.load
+    rescue Errno::ENOENT
+      skip
+    end
+    config = config_base.dup.tap do |c|
+      c['queries'] = [
+        'query_dynamic' => 'watching',
+      ]
+    end
+
+    github_client = Slahub::GithubClient.new(github_access_token: config['github_access_token'])
+    searchers = Slahub::SearchersBuilder.new(config: config, github_client: github_client).build
+
+    assert searchers.is_a?(Array)
+    assert searchers.size > 0
+    assert(searchers.all? { |s| s.is_a? Slahub::Searcher })
+  end
 end
