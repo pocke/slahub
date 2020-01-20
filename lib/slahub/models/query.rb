8be870bee94653e7a840e4ed55d4562338a19fb0
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-01-20 20:30:37 +0900

    update newest_issue_updated_at only if the query is a new record

--- lib/slahub/models/query.rb
+++ lib/slahub/models/query.rb
@@ -4,7 +4,7 @@ module Slahub
       def self.fetch_by_query(query)
         find_or_initialize_by(query: query).tap do |q|
           # TODO: 24.hour.ago is for test data, it should be 5 minutes ago or so on.
-          q.update!(newest_issue_updated_at: 24.hour.ago)
+          q.update!(newest_issue_updated_at: 24.hour.ago) if q.new_record?
         end
       end
     end
